ccb858357625d895b275ad76e9c268bcfc72f597
M-Talha183 <ullahehsan016@gmail.com>
2026-06-21 21:20:36 +0500

    add login and signup page

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
-let username = document.getElementById('username').value;
-let email = document.getElementById('email').value;
-let password = document.getElementById('password').value;
+let username = document.getElementById('username')
+let email = document.getElementById('email')
+let password = document.getElementById('password')
 let signupbtn = document.getElementById('signup-btn');
+let all_user = []
 function validateForm(){
     // if(username === '' || email === '' || password === ''){
     //     alert('Please fill in all fields');
     //     return false;
     // }
-    console.log(`${username} ${email} ${password}`);
+    // let usernameValue = username.value
+    // let emailValue = email.value
+    // let passwordValue = password.value
+    // console.log(`${usernameValue} ${emailValue} ${passwordValue}`);
+
+
+    const user = {
+        name : username.value,
+        email : email.value,
+        password : password.value
+    }
+    all_user.push(user)
+    username.value = ''
+    email.value = ''
+    password.value = ''
+
+    console.log(all_user);
 }
 
-signupbtn.addEventListener('click',validateForm())
+signupbtn.addEventListener('click',validateForm)
 
